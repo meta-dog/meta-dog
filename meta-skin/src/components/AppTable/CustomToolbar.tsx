b20d4f3fc20f2b919493b3acbb66686dc3e5b7b9
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-09-20 13:31:13 +0200

    ✨ frontend: Add refresh table button on CustomToolbar

--- meta-skin/src/components/AppTable/CustomToolbar.tsx
+++ meta-skin/src/components/AppTable/CustomToolbar.tsx
@@ -1,18 +1,30 @@
+import { Refresh } from "@mui/icons-material";
+import { IconButton, Stack } from "@mui/material";
 import { GridToolbarContainer, GridToolbarQuickFilter } from "@mui/x-data-grid";
 
+import { useAppStateContext } from "contexts";
+
 export default function CustomToolbar() {
+  const { reloadApps } = useAppStateContext();
   return (
-    <GridToolbarContainer
-      className="flex items-center gap-4 w-full"
-      sx={{ width: "100%" }}
-    >
-      <GridToolbarQuickFilter
-        debounceMs={50}
-        fullWidth
-        sx={{ width: "100%" }}
-        autoFocus
-        placeholder="Search App by name..."
-      />
+    <GridToolbarContainer>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        className="w-full gap-4"
+      >
+        <GridToolbarQuickFilter
+          debounceMs={50}
+          fullWidth
+          sx={{ width: "stretch" }}
+          autoFocus
+          placeholder="Search App by name..."
+        />
+        <IconButton onClick={() => reloadApps()} color="secondary">
+          <Refresh />
+        </IconButton>
+      </Stack>
     </GridToolbarContainer>
   );
 }
