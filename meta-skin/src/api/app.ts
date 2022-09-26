25542200a37dbc44fffe043c18e89111af2feb1d
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-09-26 14:21:31 +0200

    ♻️ Simplify Backend logic

--- meta-skin/src/api/app.ts
+++ meta-skin/src/api/app.ts
@@ -1,13 +1,44 @@
-import { AppAM } from "./apiModel";
-import apiCall from "./baseApi";
-import { mapAppAMToVM } from "./mappers";
+import { StatusCodes } from "http-status-codes";
 
-export default async function readApps() {
+import { AppAM, ReadReferralAM } from "./apiModel";
+import apiCall from "./baseApi";
+import { mapAppAMToVM, mapReadReferralAMToVM } from "./mappers";
+import { CreateReferralVM } from "./viewModel";
+
+export async function readApps() {
   const readAppsURL = `apps`;
   try {
     const { data } = await apiCall("GET", readAppsURL);
     return (data as AppAM[]).map(mapAppAMToVM);
   } catch (error) {
     throw new Error(`Error getting Apps: ${error}`);
+  }
+}
+
+export async function createReferral({ advocateId, appId }: CreateReferralVM) {
+  const createReferralURL = `app/${appId}/queue/${advocateId}`;
+  try {
+    const { status } = await apiCall("POST", createReferralURL);
+    if (status === StatusCodes.CREATED) return appId;
+    throw new Error("generic");
+  } catch (error) {
+    // eslint-disable-next-line no-unsafe-optional-chaining
+    const { status } = (error as any)?.response;
+    const code = Number.parseInt(status, 10);
+    if (code === StatusCodes.UNPROCESSABLE_ENTITY) {
+      throw new Error("unprocessable");
+    }
+    if (code === StatusCodes.CONFLICT) throw new Error("conflict");
+    throw new Error("generic");
+  }
+}
+
+export async function readReferral(appId: string) {
+  const readReferralURL = `app/${appId}/referral`;
+  try {
+    const { data } = await apiCall("GET", readReferralURL);
+    return mapReadReferralAMToVM(data as ReadReferralAM);
+  } catch (error) {
+    throw new Error(`Error creating referral: ${error}`);
   }
 }
--- meta-skin/src/api/index.ts
+++ meta-skin/src/api/index.ts
@@ -1,6 +1,5 @@
 export type { AppAM, ReadReferralAM } from "./apiModel";
 export type { AppVM, CreateReferralVM, ReadReferralVM } from "./viewModel";
 
-export { default as readApps } from "./app";
-export { createReferral, readReferral } from "./referral";
+export { readApps, createReferral, readReferral } from "./app";
 export { mapAppAMToVM, mapReadReferralAMToVM } from "./mappers";
