6d1d87420bd4822e0450b01aeae5e3634f397e96
David Camara Iglesias <david.c.iglesias@gmail.com>
2022-09-26 02:50:24 +0200

    🚸 Reload apps on close dialog

--- meta-skin/src/components/AppTable/CreateAppDialog.tsx
+++ meta-skin/src/components/AppTable/CreateAppDialog.tsx
@@ -35,7 +35,8 @@ import {
 const LIMIT = 30 as const;
 
 export default function CreateAppDialog() {
-  const { openCreateDialog, setOpenCreateDialog } = useAppStateContext();
+  const { openCreateDialog, setOpenCreateDialog, reloadApps } =
+    useAppStateContext();
 
   const { t } = useTranslation("appTableCreateAppDialog");
 
@@ -226,10 +227,15 @@ export default function CreateAppDialog() {
   const context = currAdvocateId === null ? "" : "with-advocate";
   const title = t("title", { context, currAdvocateId });
 
+  const handleClose = () => {
+    setOpenCreateDialog(false);
+    reloadApps();
+  };
+
   return (
     <Dialog
       open={openCreateDialog}
-      onClose={() => setOpenCreateDialog(false)}
+      onClose={handleClose}
       sx={{ "& .MuiPaper-root": { width: "95vw", maxWidth: "650px" } }}
     >
       <DialogTitle textAlign="center">{title}</DialogTitle>
